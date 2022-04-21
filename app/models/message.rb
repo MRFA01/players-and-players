b904be02960a3fb7da7f1ce45bf51fc1743f737f
moises arias <moises01arias@gmail.com>
2022-04-21 16:48:45 -0300

    model message

---
+++ app/models/message.rb
@@ -0,0 +1,10 @@
+class Message < ApplicationRecord
+def change
+    create_table :messages do |t|
+      t.string :content
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
