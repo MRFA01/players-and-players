e481d722bfbdcd9adab53ccf3eacf245baaae843
moises arias <moises01arias@gmail.com>
2022-05-06 10:32:56 -0300

    Update View

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,105 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require 'open-uri'
+require 'json'
+
+
+case Rails.env
+    when 'development'
+      User.destroy_all
+      puts ":reciclar: Destroyed all Users"
+      puts 'Creating users...'
+
+      admin = User.new(
+                    first_name: "Admin",
+                    last_name: "Admin",
+                    age: 58,
+                    nickname: "Admin",
+                    email: "test@test.com",
+                    password: "password",
+                    # photo: image_tag "profile.png"
+                    )
+      admin.save!
+
+      9.times do |i|
+        puts i
+        first_name = Faker::Name.first_name
+        last_name = Faker::Name.last_name
+        file = URI.open('https://i.pravatar.cc/300')
+        user = User.new(
+                    first_name: first_name,
+                    last_name: last_name,
+                    age: rand(16..55),
+                    nickname: Faker::Internet.slug(words: first_name.downcase + ' ' + last_name.downcase),
+                    email: Faker::Internet.email(name: first_name.downcase + ' ' +  last_name.downcase, separators: '_'),
+                    password: "123456",
+                    )
+
+        user.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+        user.save!
+        #chat = Chatroom.new
+        #chat.user = user
+        #chat.save!
+      end
+    when 'test'
+    # test-specific seeds ...
+    # (Consider having your tests set up the data they need
+    # themselves instead of seeding it here!)
+    when 'production'
+      User.destroy_all
+      puts ":reciclar: Destroyed all Users"
+      puts 'Creating users...'
+
+      admin = User.new(
+                    first_name: "Admin",
+                    last_name: "Admin",
+                    age: 58,
+                    nickname: "Admin",
+                    email: "test@test.com",
+                    password: "password",
+                    # photo: image_tag "profile.png"
+                    )
+      admin.save!
+
+      9.times do |i|
+        puts i
+        first_name = Faker::Name.first_name
+        last_name = Faker::Name.last_name
+        file = URI.open('https://i.pravatar.cc/300')
+        user = User.new(
+                    first_name: first_name,
+                    last_name: last_name,
+                    age: rand(16..55),
+                    nickname: Faker::Internet.slug(words: first_name.downcase + ' ' + last_name.downcase),
+                    email: Faker::Internet.email(name: first_name.downcase + ' ' +  last_name.downcase, separators: '_'),
+                    password: "123456",
+                    )
+
+        user.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+        user.save!
+        #chat = Chatroom.new
+        #chat.user = user
+        #chat.save!
+      end
+end
+
+puts "Cleaning up database..."
+Game.destroy_all
+puts "Database cleaned"
+
+url = "https://www.freetogame.com/api/games"
+  games = JSON.parse(open("#{url}").read)
+  games.each do |game|
+    platform = Platform.find_or_create_by(name: game['platform'])
+    puts "Creating #{game['title']}"
+    Game.create(
+      name: game['title'],
+      summary: game['short_description'],
+      thumbnail: game['thumbnail'],
+      category: game['genre'],
+      platform_id: platform.id
+    )
+  end
+puts "Games created"
